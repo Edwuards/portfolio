ac67db601f5823d88e63d8c0986ecf6e90416a1f
Cesar Edwuards <48424617+Edwuards@users.noreply.github.com>
2020-02-22 23:03:35 -0800

    working on new gradient design . Also you will have to modify the Doodle project. You have to create a Gradient object so you can better manipulate the gradient

--- public/js/index.js
+++ public/js/index.js
@@ -1343,12 +1343,36 @@
   		x: 0, y:0
   	});
 
-  	let x = window.innerWidth, y = window.innerHeight;
-  	g.context.createRadialGradient(x+200,y+200,1000,x,y,100);
-  	g.context.addColorStop(0,'white');
+  	[
+  		[
+  			{ w: window.innerWidth, h: window.innerHeight, x: 0, y:0 },
+  			(()=>{
+  				let x = window.innerWidth, y = window.innerHeight;
+  					return { createRadialGradient: [x+200,y+200,1000,x,y,100],
+  						addColorStop: [[0,'#f6e8fd'],[0.9,'#c29ad6'],[1,'#d02a86']]
+  					}
+  			})()
+  		],
+  		[
+  			{ w: window.innerWidth, h: window.innerHeight, x: 0, y:0 },
+  			(()=>{
+  				let x = 0, y = 100;
+  					return { createRadialGradient: [x,y,400,x,y,20],
+  						addColorStop: [[0,'#f5deb3b3'],[1,'#ffdc5f94']]
+  					}
+  			})()
+  		],
 
-  	g.context.addColorStop(0.5,'#b5ff10');
-  	g.context.addColorStop(1,'yellow');
+  	].forEach((args)=>{
+  		let g = doodle.graphics.create.rectangle.call(null,args[0]);
+  		g.context.createRadialGradient.apply(null,args[1].createRadialGradient);
+  		args[1].addColorStop.forEach((color)=>{ g.context.addColorStop.apply(null,color); });
+  	});
+
+
+
+
+
   });
 
 }());
