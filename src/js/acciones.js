bee79e037bb9b174cf11712e9fbc808db33f9747
Cesar Edwuards <48424617+Edwuards@users.noreply.github.com>
2020-06-08 23:02:52 -0500

    se agrego la animación 3d , se va a trabajar la parte de yo , proyectos y contacto ";

--- src/js/acciones.js
+++ src/js/acciones.js
@@ -1,36 +1,12 @@
 import { default as Vistas } from './vistas.js';
-import { default as GradientesInit } from './gradientes.js';
 
 export default function(){
   const VistasInit =  Vistas();
   const { Secciones } = VistasInit;
   const Acciones = {};
   const Elementos = {}
-  const Gradientes = GradientesInit();
 
-  Acciones.transición = (gradiente)=>{
-    if(gradiente != Gradientes.estado.activo){
-      let activo = Gradientes.estado.activo;
-      if(activo == ''){ Gradientes.acciones.activar(gradiente); }
-      else{
-        Gradientes.acciones.desactivar(activo).then(()=>{
-          Gradientes.acciones.activar(gradiente);
-        });
-      }
-    }
-
-  }
   Acciones.mostrar = {};
-  Acciones.mostrar.sección = (elemento)=>{
-    let secciones = {
-      'sobreMí':'café-claro',
-      'proyectos':'rojo-morado'
-    };
-    let r = Gradientes.elementos[secciones[elemento.attr('href')]].radials[0].radius;
-    console.log(r);
-    Acciones.transición(secciones[elemento.attr('href')]);
-
-  }
   Acciones.marcarElemento = function(){
     let elemento = $(this);
     Secciones.acciones.marcar(elemento);
--- src/js/eventos.js
+++ src/js/eventos.js
@@ -3,7 +3,7 @@ import { default as Init } from './acciones.js';
 function Eventos(){
   const {Acciones,Elementos} = Init();
   Elementos.secciones.a.on('mouseenter',Acciones.marcarElemento);
-  Elementos.secciones.a.on('click',Acciones.activar.sección);
+  // Elementos.secciones.a.on('click',Acciones.activar.sección);
 }
 
 
